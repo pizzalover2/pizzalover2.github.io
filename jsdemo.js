b09e8795c128e147d641a57655ea14aa6337a2d9
pizzalover2 <imayastukes@gmail.com>
2025-07-08 16:45:02 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -2,3 +2,11 @@ console.log("This output is from the external JS file")
 console.log("Name : Maya");
 console.log("School: Mallard Creek High School");
 console.log("Grade: Senior");
+
+var name= "Maya";
+let school = "Mallard Creek High School";
+var grade = "Senior";
+
+document.getElementById("name").innerHTML = "Name:<b>" + name + "</b>";
+document.getElementById("grade").innerHTML = "Grade:<b>" + grade + "</b>";
+document.getElementById("school").innerHTML = "School:<b>" + school + "</b>";

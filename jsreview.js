4edec5e8f8f38ec14b68e7381e31b2169148925b
pizzalover2 <imayastukes@gmail.com>
2025-07-10 16:52:55 -0400

    Update jsreview.js

--- jsreview.js
+++ jsreview.js
@@ -1,4 +1,19 @@
 var course = "Technology";
 let myName = "Maya"
 
-console.log(`"This page was created in the STEM Prep ${course} by ${myName}");
+console.log(`"This page was created in the STEM Prep ${course} by ${myName}`);
+
+alert(`"This page was created in the STEM Prep ${course} by ${myName}`);
+
+function output 
+{
+  let name = document.getElementById("name).value;
+  var school = document.getElementById("school").value;
+  let grade = document.getElementById("grade").value;  
+
+  let output = "Name: <b>" + name + "</b><br?";
+  output += "Grade: <b>" + grade + "</b><br>";
+  output += "School: <b>" + school + "</b><br>";
+
+  document.getElementById("output").innerHTML = output;
+}
